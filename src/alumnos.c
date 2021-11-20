bcd99bc40967c639c26d51cae02e8db601176d55
Mariano Finochietto <mariano.fino@gmail.com>
2021-11-20 10:37:45 -0300

    Se agregan los datos del alumno FINOCHIETTO, Mariano

--- src/alumnos.c
+++ src/alumnos.c
@@ -31,8 +31,15 @@ static const struct alumno_s ESTEBAN_VOLENTINI = {
     .documento = "23.517.968",
 };
 
+static const struct alumno_s MARIANO_FINOCHIETTO = {
+    .apellidos = "FINOCHIETTO",
+    .nombres = "Jose Mariano",
+    .documento = "34.851.197",
+};
+
 const alumno_t ALUMNOS[] = {
     &ESTEBAN_VOLENTINI,
+    &MARIANO_FINOCHIETTO
 };
 
 const int CANTIDAD_ALUMNOS = (sizeof(ALUMNOS) / sizeof(alumno_t));
